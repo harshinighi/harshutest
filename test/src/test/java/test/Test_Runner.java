e281b975736189cb15ca64288a678d431420fa1d
IBM <IBM@DEVOPS_10>
2018-07-25 15:04:49 +0530

    pushed

---
+++ test/src/test/java/test/Test_Runner.java
@@ -0,0 +1,13 @@
+package test;
+import org.junit.Test;
+public class Test_Runner {
+	
+	@Test
+
+	public void Test() {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+
+	}
+
+}
